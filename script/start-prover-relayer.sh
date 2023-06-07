5c72b2f621700d2c91236ed421d3c0faeb5f7f39
wolfderechter <wolfderechter@gmail.com>
2023-06-07 19:52:39 +0200

    update to support alpha3

--- script/start-prover-relayer.sh
+++ script/start-prover-relayer.sh
@@ -12,14 +12,14 @@ if [ "$VITE_ENABLE_PROVER" == "true" ]; then
 
     taiko-client prover \
         --l1.ws ${L1_ENDPOINT_WS} \
-        --l2.ws ws://l2_execution_engine:${PORT_L2_EXECTION_ENGINE_WS} \
+        --l2.ws ws://l2_execution_engine:8546 \
         --l1.http ${L1_ENDPOINT_HTTP} \
-        --l2.http http://l2_execution_engine:${PORT_L2_EXECTION_ENGINE_HTTP} \
+        --l2.http http://l2_execution_engine:8545 \
         --taikoL1 ${TAIKO_L1_ADDRESS} \
         --taikoL2 ${TAIKO_L2_ADDRESS} \
         --zkevmRpcdEndpoint http://zkevm-chain-prover-rpcd:${PORT_ZKEVM_CHAIN_PROVER_RPCD} \
         --zkevmRpcdParamsPath /data \
-        --l1.proverPrivKey ${VITE_L1_PRIVATE_KEY} \
+        --l1.proverPrivKey ${L1_PROVER_PRIVATE_KEY} \
         --maxConcurrentProvingJobs 1
 else
     sleep infinity
